4a4d7b02a4024055b768f727e1b857cbbe7d7936
Sergions1 <sergionunezsierra1@gmail.com>
2025-10-18 12:02:05 +0200

    Version 1 - Prueba que falla

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		assertEquals(200,cuenta.ingresar(200));
+	}
+	
+	void testRetirar() {
+		assertEquals(0,cuenta.retirar(200));
+	}
+
+}
